a39ebefff00d1becb423a67e33581e4176dd9dac
DELL <dell@example.com>
2025-02-14 12:32:43 +0530

    Initial commit - Library Management System

---
+++ default/main.java
@@ -0,0 +1,16 @@
+public class main {
+    
+}
+package defualtPackage;
+import java.sql.SQLException;
+import services.OtherServices;
+public class Main {
+	public static void main(String[] args){
+			OtherServices otherservice = new OtherServices();
+			try {
+				otherservice.indexMethod();
+			} catch (SQLException e) {
+				System.out.println(e);
+			}
+	}
+}
---
+++ services/ConnectionClass.java
@@ -0,0 +1,14 @@
+package services;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConnectionClass {
+//	private static final String className="com.mysql.cj.jdbc.Driver";
+	private static final String url="jdbc:mysql://localhost/librarymanagementsystem";
+	private static final String userName ="root";
+	private static final String passWord="anushree";
+	
+	public static Connection getConnectionMethod() throws SQLException {
+		return DriverManager.getConnection(url,userName,passWord);
+	}
+}
